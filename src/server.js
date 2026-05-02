a0ddba52e7d72bc06200fa7ed8c7d4337b4df207
Rian Lucas <rianlucas1638@gmail.com>
2026-05-02 16:38:03 -0300

    config: logs para deploy

--- src/server.js
+++ src/server.js
@@ -3,8 +3,12 @@ import { sequelize } from "./config/database.js";
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync().then(() => {
-    app.listen(PORT, () =>
-        console.log("rodando em http://localhost:" + PORT)
-    );
-});
+sequelize.sync()
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log("rodando na porta: " + PORT)
+        );
+    })
+    .catch(err => {
+        console.error("Erro no Sequelize / Banco de Dados:", err);
+    });
